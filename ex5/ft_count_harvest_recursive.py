6f8da54dc22946aae5edf79baf014a7de230a8f9
tahinaniaina01 <tahinaniaina01@gmail.com>
2026-03-01 16:11:55 +0300

    Add spacing for improved readability in ft_count_harvest_recursive function

--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,9 +1,11 @@
 def ft_count_harvest_recursive():
     days = int(input("Days until harvest: "))
+
     def recursive_print_day(d):
         if d == 0:
             return
         recursive_print_day(d - 1)
         print(f"Day {d}")
+
     recursive_print_day(days)
     print("Harvest time!")
