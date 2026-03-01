1e30c4f4df91b38ebd13da2bca31d2ab7c0cd15f
tahinaniaina01 <tahinaniaina01@gmail.com>
2026-03-01 15:41:35 +0300

    Add ft_water_reminder function to notify when plants need watering

--- ex3/ft_plant_age.py
+++ ex3/ft_plant_age.py
@@ -1,9 +1,6 @@
-def  ft_plant_age():
+def ft_plant_age():
     plant_age = int(input("Enter plant age in days: "))
     if plant_age > 60:
         print("Plant is ready to harvest!")
     else:
         print("Plant needs more time to grow.")
-
-ft_plant_age()
-ft_plant_age()
