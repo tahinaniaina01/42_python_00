d9b7a51dd878aca48e83871f6a9689971b30e5f8
tahinaniaina01 <tahinaniaina01@gmail.com>
2026-03-01 15:36:42 +0300

    Add ft_plant_age function to determine if a plant is ready for harvest based on age

---
+++ ex3/ft_plant_age.py
@@ -0,0 +1,9 @@
+def  ft_plant_age():
+    plant_age = int(input("Enter plant age in days: "))
+    if plant_age > 60:
+        print("Plant is ready to harvest!")
+    else:
+        print("Plant needs more time to grow.")
+
+ft_plant_age()
+ft_plant_age()
