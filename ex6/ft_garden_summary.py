95027d0f90202535e0a5ba11b5a2304bd93931ff
tahinaniaina01 <tahinaniaina01@gmail.com>
2026-03-01 16:08:55 +0300

    Add ft_garden_summary function to display garden name and number of plants

---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,6 @@
+def ft_garden_summary():
+    garden_name = input("Enter garden name: ")
+    nb_plants = input("Enter number of plants: ")
+    print(f"Garden: {garden_name}")
+    print(f"Plants: {nb_plants}")
+    print("tatus: Growing well!")
